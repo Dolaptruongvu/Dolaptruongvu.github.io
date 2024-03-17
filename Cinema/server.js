b036b18ad63f1fed7b887e8788f0001231c5e2aa
Dolaptruongvu <dolaptruongvu5@gmail.com>
2024-03-18 00:35:31 +0700

    first commit

---
+++ Cinema/server.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const { app} = require("./app");
+const port = process.env.PORT || 3000;
+const server = app.listen(port, () => {
+  console.log(`App running on port : ${port}`);
+});
+dotenv.config({ path: `E:/Nodejs/CINEMA/config.env` });
+
+
+const DB = process.env.DATABASE.replace('<password>',process.env.DATABASE_PASSWORD)
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB connection successful");
+  })
+  .catch((err) => {
+    console.log("Error");
+  });
+
+
+
+console.log(process.env);
+console.log(app.get("env"));
+
+process.on("uncaughtException", (err) => {
+  console.log("UNCAUGHT EXCEPTION!!, shutting down...");
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
+process.on("SIGTERM",()=>{
+  console.log('SIGTERM RECEIVED. Shutting down gracefully')
+  server.close(() => {
+    console.log('Process terminated')
+  });
+})

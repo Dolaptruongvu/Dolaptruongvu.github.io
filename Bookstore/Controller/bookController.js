50c7a457b1406854882979181676e25222a2fd0e
Urlgrim <147356147+Urlgrim@users.noreply.github.com>
2024-04-15 22:25:50 +0700

    add CRUD to bookController.js

--- Bookstore/Controller/bookController.js
+++ Bookstore/Controller/bookController.js
@@ -3,4 +3,15 @@ const { catchAsync } = require("../utils/catchAsync")
 const handlerFactory = require("./handlerFactory")
 const Book = require("../Model/bookModel")
 
+//Create books
 exports.createBook = handlerFactory.createOne(Book);
+
+//Read books
+exports.allBook = handlerFactory.getAll(Book);
+exports.oneBook = handlerFactory.getOne(Book);
+
+//Update books
+exports.updateBook = handlerFactory.updateOne(Book);
+
+//Delete books
+exports.deleteBook = handlerFactory.deleteOne(Book);

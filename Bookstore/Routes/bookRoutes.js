925cc0634350fe6e8991288913b8cc5269831ca0
Urlgrim <147356147+Urlgrim@users.noreply.github.com>
2024-04-15 22:26:06 +0700

    add CRUD routes bookRoutes.js

--- Bookstore/Routes/bookRoutes.js
+++ Bookstore/Routes/bookRoutes.js
@@ -2,9 +2,21 @@ const express = require("express");
 const router = express.Router();
 const bookController = require("../Controller/bookController")
 
+//creating book
 router 
-.route("/test")
+.route("/create")
 .post(bookController.createBook)
 
+//Get all books
+router.get("/", bookController.allBook);
+
+//Get one book
+router.get("/:id", bookController.oneBook);
+
+//Update book
+router.get("/:id", bookController.updateBook);
+
+//Delete book
+router.get("/:id", bookController.deleteBook);
 
 module.exports = router;

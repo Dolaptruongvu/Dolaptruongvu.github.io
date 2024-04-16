6a123aaa31c9dc97c538cefaceb48216226e065b
Urlgrim <147356147+Urlgrim@users.noreply.github.com>
2024-04-16 23:18:20 +0700

    Remove old var

--- Bookstore/server.js
+++ Bookstore/server.js
@@ -14,8 +14,7 @@ const DB = process.env.DATABASE.replace(
 
 mongoose
   .connect(DB, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+    
   })
   .then(() => {
     console.log("DB connection successful");

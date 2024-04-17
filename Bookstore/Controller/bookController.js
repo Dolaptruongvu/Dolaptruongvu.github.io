a21d543da131ace4254abb664369ca12b182d1e8
Urlgrim <147356147+Urlgrim@users.noreply.github.com>
2024-04-17 22:45:03 +0700

    add Filter book in bookController.js, bookRoutes.js and fix bookModel.js

--- Bookstore/Controller/bookController.js
+++ Bookstore/Controller/bookController.js
@@ -15,3 +15,23 @@ exports.updateBook = handlerFactory.updateOne(Book);
 
 //Delete books
 exports.deleteBook = handlerFactory.deleteOne(Book);
+
+//Read book by filter
+exports.filterBooksByCategory = async (req, res, next) => {
+    const { category } = req.query; // Get the category from query string
+  
+    try {
+      const query = category ? { category } : {}; // Build the query object
+      const books = await Book.find(query); // Find books matching the query
+  
+      res.status(200).json({
+        status: "success",
+        results: books.length,
+        data: {
+          books,
+        },
+      });
+    } catch (err) {
+      next(err); // Pass error to global error handler
+    }
+  };

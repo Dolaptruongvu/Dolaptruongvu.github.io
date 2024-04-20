b55f638939774ac9da8fd90ff5dfdd07059fc8ff
Urlgrim <147356147+Urlgrim@users.noreply.github.com>
2024-04-20 21:28:04 +0700

    Update viewsRoutes.js

--- Bookstore/Routes/viewsRoutes.js
+++ Bookstore/Routes/viewsRoutes.js
@@ -1,12 +1,12 @@
 const express = require("express");
-const viewsController = require("../Controller/viewsController"); // Use controllers folder
-const authController = require("../Controller/authController"); // Use controllers folder
+const viewsController = require("../Controller/viewsController"); 
+const authController = require("../Controller/authController");
 
 const router = express.Router();
 
 router.get(
   "/", 
-  authController.isLoggedIn, // Middleware to check if user is logged in
+  authController.isLoggedIn,
   viewsController.getOverview
 );
 

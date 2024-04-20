9af650bb7104a001add9035e3cf439c957408659
tangtuan19 <tangtuan1901@gmail.com>
2024-04-21 01:56:32 +0700

    add test edit

--- Bookstore/Controller/viewsController.js
+++ Bookstore/Controller/viewsController.js
@@ -1,19 +1,18 @@
 const Book = require("../Model/bookModel"); // Import the Book model
 const { catchAsync } = require("../utils/catchAsync"); // Import catchAsync helper
 
-exports.getOverview = catchAsync( async (req, res, next) => {
+exports.getOverview = catchAsync(async (req, res, next) => {
   let filter = {};
   if (req.params.bookId) {
     filter = { _id: req.params.bookId }; // Filter by book ID
   }
 
   const sort = req.query.sort ? { [req.query.sort]: 1 } : { _id: 1 }; // Simple sorting
-// Ensure the base image path is defined and accessible from the template
+  // Ensure the base image path is defined and accessible from the template
 
   const books = await Book.find(filter).sort(sort);
 
   res.status(200).render("book-filter", {
     books, // Pass the books data to the template
   });
-
 });

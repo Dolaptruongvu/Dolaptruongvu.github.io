a45240d1762240d81161e903181365826568d505
Urlgrim <147356147+Urlgrim@users.noreply.github.com>
2024-04-30 01:17:55 +0700

    Fix Book multer

--- Bookstore/Controller/bookController.js
+++ Bookstore/Controller/bookController.js
@@ -2,6 +2,25 @@ const AppError = require("../utils/appError")
 const { catchAsync } = require("../utils/catchAsync")
 const handlerFactory = require("./handlerFactory")
 const Book = require("../Model/bookModel")
+const multer = require('multer')
+
+//upload picture
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+      cb(null, 'D:/TruongVu-dev-project/Bookstore/Public/Img/Books'); //Storage place
+  },
+  filename: (req, file, cb) => {
+    const { title } = req.body; // Extract book title from request body
+    if (!title) {
+      return cb(new Error('Book title is required'));
+    }
+    const safeTitle = title.replace(/\s+/g, ''); //Remove spaces
+    cb(null, `${safeTitle}-cover.jpg`); //"-cover.jpg"
+  },
+});
+const upload = multer({ storage });
+
+exports.uploadBookCover = upload.single('bookCover'); // Change bookCover to front-end html
 
 //Create books
 exports.createBook = handlerFactory.createOne(Book);

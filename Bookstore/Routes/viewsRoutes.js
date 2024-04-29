24c368cabd8eb3aeddaeae2addb8a9226090f24d
Urlgrim <147356147+Urlgrim@users.noreply.github.com>
2024-04-30 01:14:55 +0700

    Update login page render

--- Bookstore/Routes/viewsRoutes.js
+++ Bookstore/Routes/viewsRoutes.js
@@ -3,11 +3,17 @@ const viewsController = require("../Controller/viewsController");
 const authController = require("../Controller/authController");
 
 const router = express.Router();
-
+//overview page
 router.get(
   "/", 
   authController.isLoggedIn,
   viewsController.getOverview
+);
+//login page
+router.get(
+  "/login", 
+  authController.isLoggedIn,
+  viewsController.getLogin
 );
 
 module.exports = router;

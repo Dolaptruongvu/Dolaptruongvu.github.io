d49ec16298cf724f6f79e427da3005c9a0b42192
Urlgrim <147356147+Urlgrim@users.noreply.github.com>
2024-05-02 15:54:39 +0700

    Update viewsRoutes.js for proper login view

--- Bookstore/Routes/viewsRoutes.js
+++ Bookstore/Routes/viewsRoutes.js
@@ -3,17 +3,19 @@ const viewsController = require("../Controller/viewsController");
 const authController = require("../Controller/authController");
 
 const router = express.Router();
-//overview page
-router.get(
-  "/", 
-  authController.isLoggedIn,
-  viewsController.getOverview
-);
+
 //login page
 router.get(
   "/login", 
   authController.isLoggedIn,
   viewsController.getLogin
+);
+
+//overview page
+router.get(
+  "/", 
+  authController.isLoggedIn,
+  viewsController.getOverview
 );
 
 module.exports = router;
